c19d581d674b3c2f177a80c668679a7ebbb662b3
Michael Tesar <michtesar@gmail.com>
2026-09-13 00:45:51 +0200

    add a player demo

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,35 @@
+use raylib::prelude::*;
+
+use crate::player::Player;
+
 pub mod player;
 
 fn main() {
-    println!("Hello, world!");
+    let (mut rl, thread) = raylib::init()
+        .size(640, 480)
+        .title("Greybox Demo")
+        .vsync()
+        .build();
+
+    rl.set_target_fps(120);
+
+    let mut player = Player::new(Vector2 { x: 100.0, y: 100.0 }, 100.0);
+
+    let mut player_target: Option<Vector2>;
+    while !rl.window_should_close() {
+        let dt = rl.get_frame_time();
+
+        if rl.is_mouse_button_pressed(MouseButton::MOUSE_BUTTON_LEFT) {
+            player_target = Some(rl.get_mouse_position());
+        } else {
+            player_target = None;
+        }
+
+        let mut d = rl.begin_drawing(&thread);
+
+        d.clear_background(Color::BLACK);
+
+        player.update(dt, player_target);
+        player.draw(&mut d);
+    }
 }
